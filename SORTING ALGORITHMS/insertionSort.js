ffe61fcaacf4ad4b48b60e4c85a461e4ebf2de4f
Senadin <beganovicsenadin24@gmail.com>
2020-10-22 12:59:02 +0200

    implemented merging arrays functionality

--- SORTING ALGORITHMS/insertionSort.js
+++ SORTING ALGORITHMS/insertionSort.js
@@ -3,13 +3,17 @@ function insertionSort(arr){
   for(var i = 1; i < arr.length; i++){
     var currentVal = arr[i];
     
+    // 38
     for(var j = i - 1; j >= 0 && arr[j] > currentVal; j--){
+      // console.log(arr[j+1], arr[j]);
+      // console.log('-------')
       arr[j+1] = arr[j];
+      //console.log(arr);
+      // [1, 2, 9, 76, 4]
     }
     arr[j+1] = currentVal;
-    console.log(arr); 
   }
   return arr;
 }
 
-insertionSort([2,1,9,76,4])
+insertionSort([3,44,38,5,47,15])
