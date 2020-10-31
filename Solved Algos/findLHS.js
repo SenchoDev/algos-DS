40177b8af47b7610fc66171e0b8958d9d28c0861
Senadin <beganovicsenadin24@gmail.com>
2020-10-31 12:57:31 +0100

    added another solution

--- Solved Algos/findLHS.js
+++ Solved Algos/findLHS.js
@@ -15,21 +15,42 @@
 
 // One solution but not performant at all
 
-const findLHS = function (nums) {
-  let numsOccuranceCount = {};
-  let resultLength = 0;
-  for (let num of nums) {
-    numsOccuranceCount[num] = (numsOccuranceCount[num] || 0) + 1;
-  }
+// const findLHS = function (nums) {
+//   let numsOccuranceCount = {};
+//   let resultLength = 0;
+//   for (let num of nums) {
+//     numsOccuranceCount[num] = (numsOccuranceCount[num] || 0) + 1;
+//   }
 
-  for (const [key1, value1] of Object.entries(numsOccuranceCount)) {
-    for (const [key2, value2] of Object.entries(numsOccuranceCount)) {
-      if (key1 - key2 === Math.abs(1) && value1 + value2 > resultLength) {
-        resultLength = value1 + value2;
+//   for (const [key1, value1] of Object.entries(numsOccuranceCount)) {
+//     for (const [key2, value2] of Object.entries(numsOccuranceCount)) {
+//       if (key1 - key2 === Math.abs(1) && value1 + value2 > resultLength) {
+//         resultLength = value1 + value2;
+//       }
+//     }
+//   }
+//   return resultLength === 0 ? 0 : resultLength;
+// };
+
+// console.log(findLHS([1, 3, 2, 2, 5, 2, 3, 7]))
+
+const findLHS = function(nums){
+  let res = 0;
+  for(let i = 0; i < nums.length; i++){
+    let count = 0;
+    let flag = false;
+    for(let j = 0; i < nums.length; i++){
+      if(nums[j] === nums[i]) {
+        count++;
+      } else if(nums[j] + 1 === nums[i]){
+        count++;
+        flag=true;
       }
     }
+    if(flag){
+      res = Math.max(count, res);
+    }
   }
-  return resultLength === 0 ? 0 : resultLength;
-};
-
-console.log(findLHS([1, 3, 2, 2, 5, 2, 3, 7]));
+  return res;
+}
+console.log(findLHS([1, 3, 2, 2, 5, 2, 3, 7]))
