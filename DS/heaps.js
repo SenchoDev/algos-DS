1a79a463ebd9c40bb7ce52cb55ca9454f9185962
Senadin <beganovicsenadin24@gmail.com>
2020-11-17 11:59:00 +0100

    implemeted priority queue using heaps

--- DS/heaps.js
+++ DS/heaps.js
@@ -1,3 +1,4 @@
+// heaps are used for priority queues/
 class MaxBinaryHeap {
   constructor() {
     this.values = [41, 39, 33, 18, 27, 12];
@@ -22,9 +23,11 @@ class MaxBinaryHeap {
   extractMax(){
     const max = this.values[0];
     const end = this.values.pop();
-    this.values[0] = end;
-    // trickle down
-    this.sinkDown();
+    if(this.values.length > 0){
+      this.values[0] = end;
+      // trickle down
+      this.sinkDown();
+    }
     return max;
   }
   sinkDown(){
@@ -45,7 +48,7 @@ class MaxBinaryHeap {
       }
       if(rightChildIdx < length){
         rightChild = this.values[rightChildIdx];
-        if(swap === null && rightChild > element){
+        if((swap === null && rightChild > element) || (swap !== null && rightChild > leftChild)){
           swap = rightChildIdx
         }
       }
@@ -58,4 +61,82 @@ class MaxBinaryHeap {
 }
 
 let heap = new MaxBinaryHeap();
-heap.insert(55);
+
+class Node {
+  constructor(val, priority) {
+    this.val = val;
+    this.priority = priority;
+
+  }
+
+}
+
+class PriorityQueue {
+  constructor() {
+    this.values = [];
+  }
+  enqueue(val, priority) {
+    let newNode = new Node(val, priority);
+    this.values.push(newNode);
+    this.bubbleUp();
+  }
+  bubbleUp() {
+    let idx = this.values.length - 1;
+    const element = this.values[idx];
+    while (idx > 0) {
+      let parentIdx = Math.floor((idx - 1) / 2);
+      let parent = this.values[parentIdx];
+      if (element.priority >= parent.priority) break;
+
+      this.values[parentIdx] = element;
+      this.values[idx] = parent;
+      idx = parentIdx;
+    }
+  }
+  dequeue(){
+    const min = this.values[0];
+    const end = this.values.pop();
+    if(this.values.length > 0){
+      this.values[0] = end;
+      // trickle down
+      this.sinkDown();
+    }
+    return min;
+  }
+  sinkDown(){
+    let idx = 0;
+    const length = this.values.length;
+    const element = this.values[0];
+    while(true){
+      let leftChildIdx = 2 * idx + 1;
+      let rightChildIdx = 2 * idx + 2;
+      let leftChild, rightChild;
+      let swap = null;
+
+      if(leftChildIdx < length){
+        leftChild = this.values[leftChildIdx];
+        if(leftChild.priority < element.priority){
+          swap = leftChildIdx
+        }
+      }
+      if(rightChildIdx < length){
+        rightChild = this.values[rightChildIdx];
+        if((swap === null && rightChild.priority < element.priority) || (swap !== null && rightChild.priority < leftChild.priority)){
+          swap = rightChildIdx
+        }
+      }
+
+
+
+      if(swap === null) break;
+    }
+  }
+  
+}
+let ER = new PriorityQueue();
+ER.enqueue("common cold", 1);
+ER.enqueue("gunshot wound", 5)
+ER.enqueue("high fever", 2)
+
+
+
