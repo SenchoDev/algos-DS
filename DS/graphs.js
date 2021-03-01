ffc6e3fb2822ef528f06f542568f3d217f60faed
Senadin <beganovicsenadin24@gmail.com>
2021-03-01 18:46:52 +0100

    added depthfirstrecursive and depthfirstiterative search solution

--- DS/graphs.js
+++ DS/graphs.js
@@ -12,22 +12,79 @@ class Graph {
   }
 
   removeEdge(v1, v2) {
-    this.adjacencyList[v1] = this.adjacencyList[v1].filter(
-      (v) => v !== v2
-    );
-    this.adjacencyList[v2] = this.adjacencyList[v2].filter(
-      (v) => v !== v1
-    );
+    this.adjacencyList[v1] = this.adjacencyList[v1].filter((v) => v !== v2);
+    this.adjacencyList[v2] = this.adjacencyList[v2].filter((v) => v !== v1);
   }
 
-  removeVertex(vertex){
-    while(this.adjacencyList[vertex].length){
+  removeVertex(vertex) {
+    while (this.adjacencyList[vertex].length) {
       const adjacentVertex = this.adjacencyList[vertex].pop();
       this.adjacencyList(vertex, adjacentVertex);
     }
   }
+
+  detphFirstRecursive(start) {
+    const result = [];
+    const visited = {};
+    const adjacencyList = this.adjacencyList;
+
+    (function dfs(vertex) {
+      if (!vertex) return null;
+      visited[vertex] = true;
+      result.push(vertex);
+
+      adjacencyList[vertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          return dfs(neighbor);
+        }
+      });
+    })(start);
+  }
+
+  detphFirstIterative(start) {
+    const stack = [start];
+    const result = [];
+    const visited = {};
+    let currentVertex;
+
+    visited[start] = true;
+    while (stack.length) {
+      console.log(stack);
+      currentVertex = stack.pop();
+      result.push(currentVertex);
+
+      this.adjacencyList[currentVertex].forEach((neighbor) => {
+        if (!visited[neighbor]) {
+          visited[neighbor] = true;
+          stack.push(neighbor);
+        }
+      });
+    }
+
+    return result;
+  }
 }
 
 let g = new Graph();
-g.addVertex("Dallas");
-g.addVertex("Tokyo");
+g.addVertex("A");
+g.addVertex("B");
+g.addVertex("C");
+g.addVertex("D");
+g.addVertex("E");
+g.addVertex("F");
+
+g.addEdge("A", "B");
+g.addEdge("A", "C");
+g.addEdge("B", "D");
+g.addEdge("C", "E");
+g.addEdge("D", "E");
+g.addEdge("D", "F");
+g.addEdge("E", "F");
+
+//     A
+//   /   \
+//  B     C
+//  \     /
+//   D---E
+//   \  /
+//    F
